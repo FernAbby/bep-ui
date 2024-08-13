19978fe952c601cb66127016b94d3fb1cd46c506
文珍 <wenzhen@yuanxinjituan.com>
2024-08-13 10:50:51 +0800

    feat: SelectRender支持远程搜索

--- packages/bep-ui/components/FormPlus/renderers/Select/index.vue
+++ packages/bep-ui/components/FormPlus/renderers/Select/index.vue
@@ -1,11 +1,11 @@
 <template>
   <el-select
+    v-bind="renderOptions"
+    v-model="model"
     :placeholder="placeholder"
     clearable
     filterable
     placement="bottom-start"
-    v-bind="renderOptions"
-    v-model="model"
     :disabled="disabled"
   >
     <el-option
@@ -18,8 +18,8 @@
 </template>
 <script lang="ts" setup>
   import { ElSelect, ElOption } from 'element-plus'
-  import { computed } from 'vue'
-  import { isEmpty, omit } from 'biz-gadgets'
+  import { computed, onMounted, ref, watch } from 'vue'
+  import { isEmpty, isFunction, omit } from 'biz-gadgets'
   import { rendererProps } from '../../constants/rendererProps'
   const emits = defineEmits(['update:modelValue'])
   const props = defineProps(rendererProps)
@@ -29,11 +29,31 @@
       emits('update:modelValue', isEmpty(value) ? undefined : value)
     }
   })
-  const options = computed(() => {
-    return props.field?.renderOptions?.options || []
-  })
+  const options = ref<Record<string, any>[]>([])
 
   const renderOptions = computed(() => {
-    return omit(props.field?.renderOptions || {}, ['options'])
+    return omit(props.field?.renderOptions || {}, ['options', 'request'])
+  })
+
+  watch(
+    () => props.field?.renderOptions,
+    (value) => {
+      if (value?.options) {
+        options.value = value.options || []
+      }
+    },
+    {
+      deep: true,
+      immediate: true
+    }
+  )
+
+  onMounted(() => {
+    const request = props.field?.renderOptions?.request
+    if (request && isFunction(request)) {
+      request().then((data) => {
+        options.value = data
+      })
+    }
   })
 </script>
